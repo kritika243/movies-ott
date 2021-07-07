2f39849ea68b0389aff8b42357daaeb635bcd861
Kritika <24kritikasharma24@gmail.com>
2021-07-08 01:31:18 +0530

    hero image rendered with styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from 'react'
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 // components
+import HeroImage from './HeroImage'
 
 // hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -14,7 +15,17 @@ import NoImage from '../images/no_image.jpg'
 const Home = () => {
   const { state, loading, error } = useHomeFetch()
 
-  return <div>Home Page</div>
+  return (
+    <>
+      {state.results[0] ? (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+          title={state.results[0].original_title}
+          text={state.results[0].overview}
+        />
+      ) : null}
+    </>
+  )
 }
 
 export default Home
