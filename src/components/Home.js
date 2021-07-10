2e664bc00f4069ed39bb681863439a802fcc9b97
Kritika <24kritikasharma24@gmail.com>
2021-07-10 13:09:54 +0530

    :white_check_mark: spinner component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 import HeroImage from './HeroImage'
 import Grid from './Grid'
 import Thumbnail from './Thumbnail'
+import Spinner from './Spinner'
 
 // hook
 import { useHomeFetch } from '../hooks/useHomeFetch'
@@ -40,6 +41,7 @@ const Home = () => {
           />
         ))}
       </Grid>
+      <Spinner />
     </>
   )
 }
