0c39364d9a89457fbd960ac027b7042fe60ad8b6
Kritika <24kritikasharma24@gmail.com>
2021-07-12 18:05:59 +0530

    fetching movie data from the Api using custom hooks

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 
 import Grid from './Grid'
 import Spinner from './Spinner'
 
+import { useMovieFetch } from '../hooks/useMovieFetch'
+
 import NoImage from '../images/no_image.jpg'
 
 const Movie = () => {
+  const { movieId } = useParams()
+  const { state: movie, loading, error } = useMovieFetch(movieId)
   return (
     <>
       <div>Movie</div>
