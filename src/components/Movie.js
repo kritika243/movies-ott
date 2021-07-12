57a537b3baaed8e8420b88352f32e72a2b3f9ec8
Kritika <24kritikasharma24@gmail.com>
2021-07-12 17:33:24 +0530

    impoerted necessary things for movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
-const Movie = () => <div>Movie</div>
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+
+import Grid from './Grid'
+import Spinner from './Spinner'
+
+import NoImage from '../images/no_image.jpg'
+
+const Movie = () => {
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  )
+}
 
 export default Movie
